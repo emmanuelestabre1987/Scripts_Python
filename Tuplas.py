0011fcc4725d91fdcc8d7eff84b4630cae321b00
Emmanuel <emanuel.estabre@gmail.com>
2026-02-27 13:06:35 -0300

    Agrego Tuplas

---
+++ Tuplas.py
@@ -0,0 +1,18 @@
+#---------------------------
+#TUPLAS [Coleccion de elementos inmutables y ordenados]
+#---------------------------
+
+frutas= ("Manzana","Platano","Cereza", "Manzana") # Acepta duplciados a diferencia de los sets
+numeros=(1,2,3)
+booleano=(True,False,True)
+print(frutas)
+
+# ver longitud
+
+print(len(frutas))
+
+# ver el tipo de dato
+print(type(frutas))
+
+fruta= tuple ("Manzana") # Aca al castearlo me separa letra por letra
+print(fruta)
